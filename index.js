294b4cfabd4f73fd57814a07202c2d5fd8929ef9
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-02 23:46:13 +0100

    add .env.example file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const AirtableData = require('./airtable/airtable').AirtableData
 
 const a = new AirtableData('COMMANDES')
-const b = new AirtableData('qsfmdslgkjezrmlsfk', 'azertyuiop')
-
+const b = new AirtableData('PRODUITS')
 
 a.checkData()
-a.read()
 b.checkData()
+a.read()
+b.read()
