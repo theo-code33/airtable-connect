ff472af74d74cc2ea31142457fc1fcda4f223d13
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-05 11:17:53 +0100

    add env variable to React and viteJS

--- airtable/config/config.airtable.js
+++ airtable/config/config.airtable.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const Airtable = require("Airtable")
 module.exports = class AirtableConfig {
     constructor(apiKey, baseID){
-        this.apiKey = process.env.AIRTABLE_API_KEY || apiKey
-        this.baseID = process.env.AIRTABLE_BASE_ID || baseID
+        this.apiKey = process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKey
+        this.baseID = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseID
         this.base = new Airtable({apiKey: this.apiKey}).base(this.baseID)
     }
 }
