f835798b7d80b645ffeaa11a598b846109e3fcc2
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-06 11:51:36 +0100

    [FIX] add condition if project doesn't use dotenv

--- airtable/config/config.airtable.js
+++ airtable/config/config.airtable.js
@@ -1,14 +1,18 @@
 import * as dotenv from 'dotenv'
-dotenv.config()
+const viteEnv = import.meta.env
+if(!viteEnv){
+    dotenv.config()
+}
 import Airtable from 'Airtable'
 const AirtableConfig = (() => {
-    
     let base = null;
     return new function(){
         this.getBase = (apiKeyParams, baseIdParams) => {
             if (base === null) {
-                const apiKey = process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams
-                const baseID = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams
+                const apiKeyData = !viteEnv ? process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams : viteEnv.AIRTABLE_API_KEY || apiKeyParams
+                const baseIDData = !viteEnv ? process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams : viteEnv.AIRTABLE_BASE_ID || baseIdParams
+                const apiKey = apiKeyData
+                const baseID = baseIDData
                 base = new Airtable({apiKey: apiKey}).base(baseID)
             }
             return base;
