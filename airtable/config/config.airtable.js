5fb616137fd1d0dc0aef1c16c328ecb9030c4757
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-06 11:58:50 +0100

    [FIX] change env variable to vite APP

--- airtable/config/config.airtable.js
+++ airtable/config/config.airtable.js
@@ -9,10 +9,8 @@ const AirtableConfig = (() => {
     return new function(){
         this.getBase = (apiKeyParams, baseIdParams) => {
             if (base === null) {
-                const apiKeyData = !viteEnv ? process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams : viteEnv.AIRTABLE_API_KEY || apiKeyParams
-                const baseIDData = !viteEnv ? process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams : viteEnv.AIRTABLE_BASE_ID || baseIdParams
-                const apiKey = apiKeyData
-                const baseID = baseIDData
+                const apiKey = !viteEnv ? process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams : viteEnv.VITE_AIRTABLE_API_KEY || apiKeyParams
+                const baseID = !viteEnv ? process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams : viteEnv.VITE_AIRTABLE_BASE_ID || baseIdParams
                 base = new Airtable({apiKey: apiKey}).base(baseID)
             }
             return base;
