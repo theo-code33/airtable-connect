5817ba3715a353cdc628256893dff974c73e4c7d
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-22 00:11:46 +0100

    [ADD] Create env.d.ts file

---
+++ env.d.ts
@@ -0,0 +1,17 @@
+export {};
+
+declare global {
+    interface ProcessEnv {
+        readonly AIRTABLE_API_KEY? : string;
+        readonly AIRTABLE_BASE_ID? : string;
+        readonly REACT_APP_AIRTABLE_API_KEY? : string;
+        readonly REACT_APP_AIRTABLE_BASE_ID? : string;
+    }
+    interface ImportMetaEnv {
+        readonly VITE_AIRTABLE_API_KEY? : string;
+        readonly VITE_AIRTABLE_BASE_ID? : string;
+    }
+    interface ImportMeta {
+        readonly env : ImportMetaEnv;
+    }
+}
