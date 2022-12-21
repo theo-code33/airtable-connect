15b6d54f72ef47a9ba866d75e54b14f31b463255
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-22 00:10:12 +0100

    [UPDATE] Add Type to getBase Params and add dotenv config

--- src/airtable/config/config.airtable.ts
+++ src/airtable/config/config.airtable.ts
@@ -1,14 +1,14 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 const viteEnv = import.meta.env
 if(!viteEnv){
-    dotenv.config()
+    dotenv.config();
 }
 import Airtable from 'Airtable'
 
 class AirtableConfig {
     public base : any = null;
     
-    public getBase = (apiKeyParams : String | null, baseIdParams : String | null) => {
+    public getBase = (apiKeyParams : string, baseIdParams : string) => {
         if(this.base == null){
             const apiKey = !viteEnv ? process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams : viteEnv.VITE_AIRTABLE_API_KEY || apiKeyParams
             const baseID = !viteEnv ? process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams : viteEnv.VITE_AIRTABLE_BASE_ID || baseIdParams
