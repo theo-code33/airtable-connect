255418bed3d282076bb29bd02fd15e10f0252324
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-30 12:46:27 +0100

    [FIX] Change singleton class to function

--- src/airtable/config/config.airtable.ts
+++ src/airtable/config/config.airtable.ts
@@ -5,17 +5,23 @@ if(!viteEnv){
 }
 import Airtable from 'Airtable'
 
-class AirtableConfig {
-    public base : any = null;
-    
-    public getBase = (apiKeyParams : string, baseIdParams : string) => {
-        if(this.base == null){
-            const apiKey = !viteEnv ? process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams : viteEnv.VITE_AIRTABLE_API_KEY || apiKeyParams
-            const baseID = !viteEnv ? process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams : viteEnv.VITE_AIRTABLE_BASE_ID || baseIdParams
-            this.base = new Airtable({apiKey: apiKey}).base(baseID)
-        }
-        return this.base;
+const AirtableConfig = (function() : {getBase: Function} {
+    let base : unknown | null = null;
+
+    function createBase(apiKeyParams? : string, baseIdParams?: string) : void {
+        const apiKey = !viteEnv ? process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams : viteEnv.VITE_AIRTABLE_API_KEY || apiKeyParams
+        const baseID = !viteEnv ? process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams : viteEnv.VITE_AIRTABLE_BASE_ID || baseIdParams
+        base = new Airtable({apiKey: apiKey}).base(baseID ? baseID : '')
     }
-}
+
+    return {
+        getBase: function(apiKeyParams? : string, baseIdParams? : string) {
+            if (base == null) {
+                createBase(apiKeyParams, baseIdParams)
+            }
+            return base;
+        }
+    }
+})()
 
 export { AirtableConfig }
