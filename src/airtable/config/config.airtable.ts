da50d9fb50db4365582a02acd53d39255a1b50c2
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-30 10:09:49 +0100

    Develop (#3)
    * [UPDATE] Remove os-browserify, path-browserify and dotenv-webpack packages
    
    * [FIX] Remove dotenv package

--- src/airtable/config/config.airtable.ts
+++ src/airtable/config/config.airtable.ts
@@ -1,19 +1,11 @@
-// import * as dotenv from 'dotenv';
-// const viteEnv = import.meta.env
-// dotenv.config();
-// if(!viteEnv){
-//     dotenv.config();
-// }
 import Airtable from 'airtable'
 
 const AirtableConfig = (function() : {getBase: Function} {
     let base : unknown | null = null;
 
     function createBase(apiKeyParams? : string, baseIdParams?: string) : void {
-        // const apiKey = process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams
-        // const baseID = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams
-        const apiKey = apiKeyParams
-        const baseID = baseIdParams
+        const apiKey = process.env.AIRTABLE_API_KEY || process.env.REACT_APP_AIRTABLE_API_KEY || apiKeyParams
+        const baseID = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams
         base = new Airtable({apiKey: apiKey}).base(baseID ? baseID : '')
     }
 
