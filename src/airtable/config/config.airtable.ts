7ab148c6e15f42921e7e0e3fb97456c006a4f1c5
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-03-17 23:16:53 +0100

    :bug: [CONFIG.AIRTABLE] fix bug of vite feature

--- src/airtable/config/config.airtable.ts
+++ src/airtable/config/config.airtable.ts
@@ -13,8 +13,15 @@ const AirtableConfig = (function() : {getBase: Function} {
     let base : unknown | null = null;
 
     function createBase(apiKeyParams? : string, baseIdParams?: string) : void {
-        let apiKey : string | undefined = process.env.AIRTABLE_PERSONNAL_TOKEN || process.env.REACT_APP_AIRTABLE_PERSONNAL_TOKEN || import.meta.env.VITE_AIRTABLE_PERSONNAL_TOKEN || apiKeyParams
-        let baseID : string | undefined = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || import.meta.env.VITE_AIRTABLE_BASE_ID || baseIdParams
+        let apiKey : string | undefined = ""
+        let baseID : string | undefined = ""
+        if (import.meta.env) {
+            apiKey = import.meta.env.VITE_AIRTABLE_PERSONNAL_TOKEN || apiKeyParams
+            baseID = import.meta.env.VITE_AIRTABLE_BASE_ID || baseIdParams
+        }else{
+            apiKey = process.env.AIRTABLE_PERSONNAL_TOKEN || process.env.REACT_APP_AIRTABLE_PERSONNAL_TOKEN || apiKeyParams
+            baseID = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams
+        }
         base = new Airtable({apiKey: apiKey}).base(baseID ? baseID : '')
     }
     return {
