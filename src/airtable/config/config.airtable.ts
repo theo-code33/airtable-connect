1803337336fd6989993835fda8982d3094c78079
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-03-17 22:32:23 +0100

    :sparkles: Add compatibility with Vite

--- src/airtable/config/config.airtable.ts
+++ src/airtable/config/config.airtable.ts
@@ -1,16 +1,27 @@
 import Airtable from 'airtable'
-
+if(require.main === module) {
+    require('dotenv').config()
+}
+/**
+* @name AirtableConfig
+* @description
+* This class is used to connect to Airtable automatically by .env variables or with **getBase** method with API Key and Base ID in parameters
+* @param apiKeyParams string
+* @param baseIdParams string
+* @returns {void}
+* @example
+* AirtableConfig.getBase('API_KEY', 'BASE_ID')
+*/
 const AirtableConfig = (function() : {getBase: Function} {
     let base : unknown | null = null;
 
     function createBase(apiKeyParams? : string, baseIdParams?: string) : void {
-        const apiKey = process.env.AIRTABLE_PERSONNAL_TOKEN || process.env.REACT_APP_AIRTABLE_PERSONNAL_TOKEN || apiKeyParams
-        const baseID = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || baseIdParams
+        let apiKey : string | undefined = process.env.AIRTABLE_PERSONNAL_TOKEN || process.env.REACT_APP_AIRTABLE_PERSONNAL_TOKEN || import.meta.env.VITE_AIRTABLE_PERSONNAL_TOKEN || apiKeyParams
+        let baseID : string | undefined = process.env.AIRTABLE_BASE_ID || process.env.REACT_APP_AIRTABLE_BASE_ID || import.meta.env.VITE_AIRTABLE_BASE_ID || baseIdParams
         base = new Airtable({apiKey: apiKey}).base(baseID ? baseID : '')
     }
-
     return {
-        getBase: function(apiKeyParams? : string, baseIdParams? : string) {
+        getBase: function(apiKeyParams? : string, baseIdParams? : string) : unknown | null {
             if (base == null) {
                 createBase(apiKeyParams, baseIdParams)
             }
